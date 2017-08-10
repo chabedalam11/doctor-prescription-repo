7194ab62f461478ab3b00db67c56308a319de9af
Rashidul Hasan <rashed.bitbucket@gmail.com>
2017-08-10 17:58:12 +0600

    Rashed > Progress cilic

---
+++ app/src/main/java/com/prescription/doctorprescription/profile/ClinicProfileDetailActivity.java
@@ -0,0 +1,15 @@
+package com.prescription.doctorprescription.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.prescription.doctorprescription.R;
+
+public class ClinicProfileDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinic_profile_detail);
+    }
+}
